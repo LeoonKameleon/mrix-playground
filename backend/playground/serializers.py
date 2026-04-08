36fa85a9b33f0576276a933544eeb8518726ccaf
LeoonKameleon <kacperwachala0@gmail.com>
2026-04-09 01:45:39 +0200

    feat: add register serializer to serializers

--- backend/playground/serializers.py
+++ backend/playground/serializers.py
@@ -1,4 +1,30 @@
-from rest_framework import serializers
+from rest_framework import serializers, validators
+from django.contrib.auth.models import User
 
 class CodeExecutionSerializer(serializers.Serializer):
     code = serializers.CharField(required=True, allow_blank=False)
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True, 
+        validators=[
+            validators.UniqueValidator(
+                queryset=User.objects.all(), 
+                message="E-mail address taken"
+            )
+        ]
+    )
+    
+    password = serializers.CharField(required=True, allow_blank=False, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"]
+        )
+        return user

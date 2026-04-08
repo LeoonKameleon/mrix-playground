507d72f49984b66af095af5a507bea1804f11b55
LeoonKameleon <kacperwachala0@gmail.com>
2026-04-09 01:44:53 +0200

    feat: add jwt auth urlpatterns to urls

--- backend/playground/urls.py
+++ backend/playground/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import ExecuteCodeView
+from .views import ExecuteCodeView, RegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenBlacklistView
 
 urlpatterns = [
     path("execute/", ExecuteCodeView.as_view(), name="execute-code"),
+    path("auth/register/", RegisterView.as_view()),
+    path("auth/login/", TokenObtainPairView.as_view()),
+    path("auth/refresh/", TokenRefreshView.as_view()),
+    path("auth/logout/", TokenBlacklistView.as_view())
 ]

76fffbdbb44267200a1d57d42cba494eb5d53d9d
LeoonKameleon <kacperwachala0@gmail.com>
2026-04-09 01:46:44 +0200

    feat: add register & logout views

--- backend/playground/views.py
+++ backend/playground/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
-from .serializers import CodeExecutionSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .serializers import CodeExecutionSerializer, RegisterSerializer
 from .services import execute_code
+from django.contrib.auth.models import User
 
 class ExecuteCodeView(APIView):
     permission_classes = [AllowAny]
@@ -13,4 +16,30 @@ class ExecuteCodeView(APIView):
             code = serializer.validated_data.get("code") # type: ignore
             log = execute_code(code)
             return Response(log)
-        return Response(serializer.errors, status=400)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {"message": "Account created successfully"}, 
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data.get("refresh")
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"message": "Successfully logged out."}, status=status.HTTP_200_OK)
+        except Exception:
+            return Response({"error": "Invalid token."}, status=status.HTTP_400_BAD_REQUEST)

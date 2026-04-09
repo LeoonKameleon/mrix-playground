2603ae47c18bb8771400329c67bd44d2e715b595
LeoonKameleon <kacperwachala0@gmail.com>
2026-04-09 04:37:57 +0200

    feat: define Execution model

--- backend/playground/models.py
+++ backend/playground/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Execution(models.Model):
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    code = models.TextField()
+    output = models.TextField(blank=True)
+    status = models.SmallIntegerField()
+    execution_time = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)

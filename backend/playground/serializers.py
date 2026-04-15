cbd1b748176c878f9c541147d9e4f6553d3b8ae4
LeoonKameleon <kacperwachala0@gmail.com>
2026-04-15 21:30:15 +0200

    feat: customize jwt token to return username

--- backend/playground/serializers.py
+++ backend/playground/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers, validators
 from django.contrib.auth.models import User
+from .models import Execution
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class CodeExecutionSerializer(serializers.Serializer):
     code = serializers.CharField(required=True, allow_blank=False)
@@ -28,3 +30,11 @@ class RegisterSerializer(serializers.ModelSerializer):
             password=validated_data["password"]
         )
         return user
+    
+
+class LoginSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data["username"] = self.user.username
+
+        return data
--- backend/playground/views.py
+++ backend/playground/views.py
@@ -4,7 +4,8 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from .serializers import CodeExecutionSerializer, RegisterSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import CodeExecutionSerializer, RegisterSerializer, ExecutionResultSerializer, LoginSerializer
 from .services import execute_code
 from django.contrib.auth.models import User
 
@@ -31,6 +32,9 @@ class RegisterView(APIView):
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class LoginView(TokenObtainPairView):
+    serializer_class = LoginSerializer
 
 class LogoutView(APIView):
     permission_classes = [IsAuthenticated]
